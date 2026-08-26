255ef86e5c56df6d59506a7e000c9b0c775ba332
phboy37-lab <phboy37@gmail.com>
2026-08-26 10:49:27 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -24,7 +24,7 @@
   introOverlay.addEventListener('animationend', (e) => {
     if (e.animationName === 'introOut') unlock();
   });
-  setTimeout(unlock, 7600);
+  setTimeout(unlock, 7200);
 })();
 
 // Scroll reveal for elements with class "reveal"
